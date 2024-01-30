751f864313f9f41e69bba319c9692a0a13ed6b7b
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-01-31 00:42:50 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -1,16 +1,17 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const sendCookie = (userdetail , res , message , statuscode=200)=>{
-    const token = jwt.sign({_id : userdetail._id} , process.env.JWT_SECRET);
-    res.status(statuscode).cookie("token" ,token , {
-        httpOnly : true , 
-        maxAge : 15*60*1000 ,
-        //for deploying none and true but in case of postman samesite must be lax and secure must be false 
-        samesite:process.env.NODE_ENV==="Development"?"lax":"none",//if we give strict mode . cors er credentials e true pass korleu cookie send hobe na frontend e
-        secure : process.env.NODE_ENV==="Development"?false :true, 
-
-    }).json({
-        success: true,
-        message 
+export const sendCookie = (user, res, message, statusCode = 200) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      httpOnly: true,
+      maxAge: 15 * 60 * 1000,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
-}
+    .json({
+      success: true,
+      message,
+    });
+};
